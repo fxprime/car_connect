20d013a75597de7877836b194fad77eac2fe76f5
tdnet12434 <paheyisoicus@gmail.com>
2020-08-30 22:20:12 +0700

    Try again

--- scripts/startup_launch.sh
+++ scripts/startup_launch.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 source /opt/ros/melodic/setup.bash
-source /home/odroid/car_ws/devel/setup.bash
-roslaunch car_connect sensors.launch
+source /home/odroid/car_ws/devel/setup.bash 
+gnome-terminal -- /bin/sh -c 'roscore; sleep 3; exec bash' & 
+
+gnome-terminal -- /bin/sh -c 'roslaunch car_connect sensors.launch; exec bash' & 
+gnome-terminal -- /bin/sh -c 'roslaunch car_connect car_2d_localization.launch load_state_filename:=${HOME}/Desktop/online_map_save.pbstream; exec bash' & 
+
+gnome-terminal -- /bin/sh -c 'roslaunch car_tracking all.launch; exec bash' & 
 
