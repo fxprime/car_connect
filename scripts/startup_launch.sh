73d980e3b9a3a5e7c970b045658dee9f9ec69e29
fxprime <paheyisoicus@gmail.com>
2020-08-30 22:47:09 +0700

    fix conflict

--- scripts/startup_launch.sh
+++ scripts/startup_launch.sh
@@ -2,13 +2,7 @@
  
 mate-terminal -- /bin/sh -c 'roscore; exec bash'&
 mate-terminal -- /bin/sh -c 'roslaunch --wait car_connect sensors.launch; exec bash' & 
-<<<<<<< HEAD
-mate-terminal -- /bin/sh -c 'roslaunch --wait car_connect car_2d_localization.launch load_state_filename:=${HOME}/Desktop/online_map_save.pbstream; exec bash' & 
-
-mate-terminal -- /bin/sh -c 'roslaunch --wait car_tracking all.launch; exec bash' & 
-=======
 #mate-terminal -- /bin/sh -c 'roslaunch --wait car_connect car_2d_localization.launch load_state_filename:=${HOME}/Desktop/online_map_save.pbstream; exec bash' & 
 mate-terminal -- /bin/sh -c 'roslaunch --wait car_connect car_2d_mapping.launch; exec bash' & 
->>>>>>> 736d7adc63632065820aca508cce795b54348bc1
 
 mate-terminal -- /bin/sh -c 'roslaunch --wait car_tracking all.launch; exec bash' & 
